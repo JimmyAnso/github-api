7ea367b574f1324ce43308d812301a050ade62a1
JimmyAnso <jimmy123698745@yahoo.com.tw>
2021-08-17 20:14:59 +0800

    feat：新增 Infinite Scroll 無限滾動 功能
    1. 在 mounted 註冊監聽事件 window.addEventListener("scroll", 事件處理器, true);
    2. 在 事件處理器 判斷 scrollTop、clientHeight、scrollHeight
    3. 當卷軸到達一定程度，重 call API，並且逐筆把資料 push 進 Repos (Array)

--- src/App.vue
+++ src/App.vue
@@ -40,12 +40,14 @@ export default {
   name: "App",
   data() {
     return {
-      myRepos: {},
+      myRepos: [],
     };
   },
   created() {
     this.getAPI();
-    console.log("create:");
+  },
+  mounted() {
+    window.addEventListener("scroll", this.isRefresh, true);
   },
   methods: {
     getAPI() {
@@ -57,14 +59,44 @@ export default {
       axios(options)
         .then((response) => {
           // handle success
-          console.log(response);
-          this.myRepos = response.data;
-          console.log(this.myRepos);
+          response.data.forEach(element => {this.myRepos.push(element)});
         })
         .catch(function (error) {
           // handle error
           console.log(error);
         });
+    },
+    //判斷捲軸位置，若到底則重 call this.getAPI() 將資料 push 到 myRepos
+    isRefresh() {
+      // document.documentElement：回傳目前 document 的根元素
+
+      //scrollTop：視窗頂端 到 頁面頂端 的距離
+      var scrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop;
+
+      //clientHeight：視窗的高度
+      var clientHeight =
+        document.documentElement.clientHeight || document.body.clientHeight;
+
+      //scrollHeight：頁面的高度
+      var scrollHeight =
+        document.documentElement.scrollHeight || document.body.scrollHeight;
+        
+      // scrollTop、clientHeight、scrollHeight： https://img2018.cnblogs.com/blog/1504647/201812/1504647-20181221155602294-354909265.png
+
+      
+      //滾動條件檢視
+      // console.log(scrollTop + clientHeight + "--" + scrollHeight);
+
+      if (
+        (scrollHeight - 200) <= scrollTop + clientHeight 
+      ) {
+        // isRefreshBool= false; 防止多次觸發 非同步API
+        // this.isRefreshBool = false;
+        
+        console.log('Call API ~~~');
+        this.getAPI();
+      }
     },
   },
 };
@@ -91,27 +123,36 @@ export default {
   &__logo {
     width: 300px;
     position: relative;
-    animation: picFloat 1s linear 0s infinite alternate;
+    animation: name duration timing-function delay iteration-count direction
+      fill-mode;
+    animation: picFloat 2s ease-in-out 0s infinite alternate;
     @keyframes picFloat {
       0% {
-        transform: translatey(-10px);
+        transform: translatey(-10px) rotate(5deg);
+        // transform: rotate(5deg);
+      }
+      25% {
+        transform: translatey(-5px) rotate(-5deg);
+      }
+      75% {
+        transform: translatey(5px) rotate(5deg);
       }
       100% {
-        transform: translatey(10px);
+        transform: translatey(10px) rotate(-5deg);
+        // transform: rotate(-5deg);
       }
     }
-    &:hover{
+    &:hover {
       animation: picShake 0.2s linear 0.3s infinite alternate;
-    @keyframes picShake {
-      0% {
-        transform: rotate(5deg);
-      }
-      100% {
-        transform: rotate(-5deg);
+      @keyframes picShake {
+        0% {
+          transform: rotate(5deg);
+        }
+        100% {
+          transform: rotate(-5deg);
+        }
       }
     }
-    }
-
   }
 }
 .list {
