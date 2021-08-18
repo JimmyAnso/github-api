47cb9f2f1739787939bec185d52a6968cb023085
JimmyAnso <jimmy123698745@yahoo.com.tw>
2021-08-18 18:05:53 +0800

    feat：Parallax Scrolling 視差滾動 功能、新增 profile 區塊、
    1. 新增 profile 區塊，多接一條API getProfile( )
    2. 新增 backToTop 功能
    3. 增加 background-attachment 效果
    4. 增加 list 的 Demo網址
    5. 另外新增10張圖做 視差滾動 - 冒泡效果

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main">
+  <div class="main" id="main">
     <a href="https://github.com/JimmyAnso" target="_blank">
       <img
         class="main__logo"
@@ -7,7 +7,35 @@
         src="./assets/Octocat.png"
       />
     </a>
+
+    <div class="profile">
+      <img class="profile__img" :src="profile.avatar_url" alt="" />
+      <h1 class="profile__name">{{ profile.login }}</h1>
+      <p class="profile__follow">
+        <img src="./assets/group.png" alt="" />
+        followers：{{ profile.followers }} ● following：{{ profile.following }}
+      </p>
+      <p>
+        <img src="./assets/url.png" alt="" /><a
+          href="https://github.com/JimmyAnso"
+          target="_blank"
+          >https://github.com/JimmyAnso</a
+        >
+      </p>
+      <p>
+        <img src="./assets/message.png" alt="" /><a
+          href="jimmy123698745@yahoo.com.tw"
+          target="_blank"
+          >jimmy123698745@yahoo.com.tw</a
+        >
+      </p>
+    </div>
+
     <ul class="list" v-for="repo in myRepos" :key="repo">
+      <li>
+        <div class="attachment"></div>
+      </li>
+
       <li class="list__li">
         <span class="list__li--title">專案名稱：</span>
         <span class="list__li--data">{{ repo.name }}</span>
@@ -23,6 +51,12 @@
         </span>
       </li>
       <li class="list__li">
+        <span class="list__li--title">Demo網址：</span>
+        <span class="list__li--data">
+          <a :href="repo.html_url" target="_blank">{{ repo.homepage }}</a>
+        </span>
+      </li>
+      <li class="list__li">
         <span class="list__li--title">建立時間：</span>
         <span class="list__li--data">{{ repo.created_at }}</span>
       </li>
@@ -31,6 +65,19 @@
         <span class="list__li--data">{{ repo.updated_at }}</span>
       </li>
     </ul>
+    <a href="#main" class="back-to-top"></a>
+
+    <img class="flot flot__1" src="./assets/daftpunktocat-guy.gif" alt="" />
+    <img class="flot flot__2" src="./assets/daftpunktocat-thomas.gif" alt="" />
+    <img class="flot flot__3" src="./assets/Octocat-ironcat.png" alt="" />
+    <img class="flot flot__4" src="./assets/Octocat-momtocat.png" alt="" />
+    <img class="flot flot__5" src="./assets/Octocat-poptocat.png" alt="" />
+    <img class="flot flot__6" src="./assets/Octocat-spidertocat.png" alt="" />
+    <img class="flot flot__7" src="./assets/Octocat-starwar.png" alt="" />
+    <img class="flot flot__8" src="./assets/Octocat-waldocat.png" alt="" />
+    <img class="flot flot__9" src="./assets/Octocat-goretocat.png" alt="" />
+    <img class="flot flot__10" src="./assets/Octocat-riddlocat.png" alt="" />
+
   </div>
 </template>
 
@@ -41,15 +88,42 @@ export default {
   data() {
     return {
       myRepos: [],
+      profile: {},
     };
   },
   created() {
     this.getAPI();
+    this.getProfile();
   },
   mounted() {
     window.addEventListener("scroll", this.isRefresh, true);
+
+    const flots = document.querySelectorAll(".flot");
+
+    flots.forEach(flot=>{
+      // 隨機水平位置
+      flot.style.left = `${Math.random() * 80}% `;
+      // 使用 dataset 給予隨機滾動速率
+      flot.dataset.speed = 0 - Math.random()*5
+      });
   },
   methods: {
+    getProfile() {
+      const options = {
+        method: "GET",
+        url: "https://api.github.com/users/JimmyAnso",
+      };
+      // axios(options);
+      axios(options)
+        .then((response) => {
+          // handle success
+          this.profile = response.data;
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        });
+    },
     getAPI() {
       const options = {
         method: "GET",
@@ -59,7 +133,9 @@ export default {
       axios(options)
         .then((response) => {
           // handle success
-          response.data.forEach(element => {this.myRepos.push(element)});
+          response.data.forEach((element) => {
+            this.myRepos.push(element);
+          });
         })
         .catch(function (error) {
           // handle error
@@ -68,6 +144,17 @@ export default {
     },
     //判斷捲軸位置，若到底則重 call this.getAPI() 將資料 push 到 myRepos
     isRefresh() {
+
+      const flots = document.querySelectorAll(".flot");
+
+      flots.forEach(flot => {
+        flot.style.transform = 
+          `translateY( 
+              ${window.pageYOffset * Number(flot.dataset.speed)}px
+          )`;
+        }
+      );
+
       // document.documentElement：回傳目前 document 的根元素
 
       //scrollTop：視窗頂端 到 頁面頂端 的距離
@@ -81,20 +168,17 @@ export default {
       //scrollHeight：頁面的高度
       var scrollHeight =
         document.documentElement.scrollHeight || document.body.scrollHeight;
-        
+
       // scrollTop、clientHeight、scrollHeight： https://img2018.cnblogs.com/blog/1504647/201812/1504647-20181221155602294-354909265.png
 
-      
       //滾動條件檢視
       // console.log(scrollTop + clientHeight + "--" + scrollHeight);
 
-      if (
-        (scrollHeight - 200) <= scrollTop + clientHeight 
-      ) {
+      if (scrollHeight - 200 <= scrollTop + clientHeight) {
         // isRefreshBool= false; 防止多次觸發 非同步API
         // this.isRefreshBool = false;
-        
-        console.log('Call API ~~~');
+
+        console.log("Call API ~~~");
         this.getAPI();
       }
     },
@@ -117,10 +201,13 @@ export default {
   margin: 0;
   padding: 0;
   list-style: none;
+  scroll-behavior: smooth;
 }
 .main {
   margin: auto;
+  position: relative;
   &__logo {
+    margin-bottom: 30px;
     width: 300px;
     position: relative;
     animation: name duration timing-function delay iteration-count direction
@@ -129,7 +216,6 @@ export default {
     @keyframes picFloat {
       0% {
         transform: translatey(-10px) rotate(5deg);
-        // transform: rotate(5deg);
       }
       25% {
         transform: translatey(-5px) rotate(-5deg);
@@ -139,7 +225,6 @@ export default {
       }
       100% {
         transform: translatey(10px) rotate(-5deg);
-        // transform: rotate(-5deg);
       }
     }
     &:hover {
@@ -155,9 +240,40 @@ export default {
     }
   }
 }
+.profile {
+  border: 5px solid black;
+  border-radius: 20px;
+  padding: 50px 0;
+  box-shadow: 0px 0px 10px 0px black;
+  margin: 0 10px;
+  &__img {
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    margin-bottom: 50px;
+  }
+  p {
+    margin-top: 20px;
+    img {
+      width: 20px;
+      height: 20px;
+      margin: 0 10px;
+      vertical-align: bottom;
+    }
+    a {
+      color: white;
+      text-decoration: none;
+      &:hover {
+        color: skyblue;
+      }
+    }
+  }
+}
 .list {
   // width: 60%;
   margin: 50px auto;
+  position: relative;
+  z-index: 99;
   & ~ .list::before {
     content: "";
     width: 100%;
@@ -186,6 +302,57 @@ export default {
       }
     }
   }
+}
+.attachment {
+  background: url("./assets/github-octocat-fire.png");
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: auto;
+  height: 600px;
+  margin: 0px 20px;
+  opacity: 0.1;
+  filter: blur(2px);
+  animation: bgFloat 2s ease-in-out 0s infinite alternate;
+  @keyframes bgFloat {
+    0% {
+      transform: translatey(-20px);
+    }
+    100% {
+      transform: translatey(20px);
+    }
+  }
+}
+.back-to-top {
+  background: #233241;
+  width: 50px;
+  height: 50px;
+  position: fixed;
+  bottom: 50px;
+  right: 100px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px 1px #233241;
+  // position: relative;
+  &:hover {
+    transform: translateY(-5px);
+  }
+  &::before {
+    content: "";
+    width: 15px;
+    height: 15px;
+    border-left: 5px solid black;
+    border-top: 5px solid black;
+    position: absolute;
+    top: 50%;
+    right: 50%;
+    transform: translate(50%, -25%) rotate(45deg);
+  }
+}
+.flot {
+  position: absolute;
+  // top: 100vh;
+  bottom: 0;
+  width: 100px;
 }
 
 // breakpoints
